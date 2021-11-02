036f4dc1df5e7e2e0407398a4e4f0fa0721dc2b3
Victorius19 <post@victorius.ru>
2021-11-02 17:42:49 +0000

    Добавлены комментарии к решению

--- terver3.cpp
+++ terver3.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-	cout << "Пункт 1a) " << one(0, 2, 0) << endl;
-	cout << "Пункт 1б) " << one(0, 2, 1) << endl;
+	cout << "Пункт 1a) " << one(0, 4, 0) << endl;
+	cout << "Пункт 1б) " << one(0, 4, 1) << endl;
 	cout << "Пункт 2a) " << two(4, 0) << endl;
 	cout << "Пункт 2б) " << two(4, 1) << endl;
-	cout << "Пункт 3a) " << three(2, 0) << endl;
-	cout << "Пункт 3б) " << three(2, 1) << endl;
+	cout << "Пункт 3a) " << three(1, 0) << endl;
+	cout << "Пункт 3б) " << three(1, 1) << endl;
 }
